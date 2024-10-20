aaa23e64d3a7c62cd45c4691822cf9c6666d0706
AdamJaworski <jaworskiadam0203@gmail.com>
2024-10-21 00:48:26 +0200

    update

--- static version.py
+++ static version.py
@@ -1,9 +1,20 @@
 import soundfile as sf
 import sounddevice as sd
+import numpy as np
+
+# fi(t) = fc + deltat * m(t)
+# delta t wynika z zasady Carsona
 
 x, fs = sf.read('')
-
+dt = 1 / fs
+Nx = len(x)
+t = dt * np.arange(Nx)
 # demod - as you wish
+
+
+# FM demod
+fi3 = (1 / (2 * np.pi)) * np.angle(x[2:] * np.conj(x[:-2])) / (2 * dt)
+fi3 = np.append(fi3, 0)
 
 
 # play sound
